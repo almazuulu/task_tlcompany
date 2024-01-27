77450743e851fd9a7e95e3dde36c06b997ec3fdb
almazuulu <almazuulu@gmail.com>
2024-01-28 01:33:51 +0800

     Обновлены докерфайлы и маленький фикс

--- weatherproject/urls.py
+++ weatherproject/urls.py
@@ -21,7 +21,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('weather/', views.get_weather, name='weather'),
+    path('weather', views.get_weather, name='weather'),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
