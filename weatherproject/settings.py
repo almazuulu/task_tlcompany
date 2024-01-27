e7871d168953a575f780095083070c19442a903c
almazuulu <almazuulu@gmail.com>
2024-01-28 02:32:08 +0800

    Изменение файла settings.py

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tgtvg6sh*6ie6&0g-o(1vc3f$jdd+-*@d6hpa4s%d3t7x+f&8k'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['*', '0.0.0.0:8000']
-
 
 # Application definition
 
